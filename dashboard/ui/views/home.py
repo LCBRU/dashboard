19622e1b8a0026812c5ebc808911e119bc575d6f
Richard Bramley <rabramley@gmail.com>
2025-03-12 16:38:23 +0000

    Example plot using matplotlib

--- dashboard/ui/views/home.py
+++ dashboard/ui/views/home.py
@@ -1,10 +1,12 @@
+import io
 from dashboard.model.projects import Project
 from .. import blueprint
-from flask import render_template, request
+from flask import render_template, request, send_file
 from lbrc_flask.forms import SearchForm
 from lbrc_flask.database import db
 from sqlalchemy import select
-
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 @blueprint.route("/")
 def index():
@@ -22,3 +24,46 @@ def index():
         projects=projects,
         search_form=search_form,
     )
+
+@blueprint.route("/plothole")
+def plothole():
+    return render_template('ui/plothole.html')
+
+@blueprint.route("/plot")
+def plot():
+    return send_file(
+        plot_points(100),
+        as_attachment=False,
+        download_name='plot.png',
+        max_age=0,
+        mimetype='image/png',
+    )
+
+
+def plot_points(points):
+    fig = Figure()
+    FigureCanvas(fig)
+
+    ax = fig.add_subplot(111)
+
+    ax.scatter([1,2,3], [3,2,1])
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_title(f'There are {points} data points!')
+    ax.grid(True)
+
+    img = io.BytesIO()
+    fig.savefig(img, format='png')
+    img.seek(0)
+    #clip off the xml headers from the image
+    # svg_img = '<svg' + img.getvalue().split('<svg')[1]
+    # unaltered_image = img.getvalue()
+
+    # print('@'*100)
+    # print(svg_img == unaltered_image)
+
+    # print(unaltered_image)
+    # print('@'*100)
+    
+    return img
